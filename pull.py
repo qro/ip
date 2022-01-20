a62b5b61300253aa3a4c502f8114a0b83f53ab00
Laura Clark <afqro@outlook.com>
2022-01-20 19:38:21 +0000

    ✨ + Add object orientation

--- pull.py
+++ pull.py
@@ -2,3 +2,11 @@ import os, sys, time, subprocess, json, socket
 from urllib.request import urlopen
 from concurrent import features
 
+client = input('')
+
+class IP():
+    def __init__(self):
+        self.client = client
+
+if __name__ == '__main__':
+    IP().main()
