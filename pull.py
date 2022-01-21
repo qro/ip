2d39c2d5d39d45d6cf4fd634c6deb5f6e552e14a
Laura Clark <afqro@outlook.com>
2022-01-21 00:24:27 +0000

    ✨ + Add `main()`

--- pull.py
+++ pull.py
@@ -1,12 +1,16 @@
-import os, sys, time, subprocess, json, socket
+import os, sys, time, subprocess, json
 from urllib.request import urlopen
 from concurrent import features
 
-client = input('')
+client = input('\n [?] Enter an IP address: ')
 
 class IP():
     def __init__(self):
         self.client = client
+
+    def main():
+        print('\n [1] ICMP Ping, [2] IP lookup, [3] Local IP\n')
+        option = int(input('\n [?] Option: '))
 
 if __name__ == '__main__':
     IP().main()
