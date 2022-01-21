245e8c702b0960f298903b44a198ab3cadc2727e
Laura Clark <afqro@outlook.com>
2022-01-21 23:53:58 +0000

    ✨ + Add ip validation

--- pull.py
+++ pull.py
@@ -2,15 +2,38 @@ import os, sys, time, subprocess, json
 from urllib.request import urlopen
 from concurrent import features
 
-client = input('\n [?] Enter an IP address: ')
-
 class IP():
     def __init__(self):
         self.client = client
 
-    def main():
+    def valid(self): # stack
+        i = 0
+        valid = True
+        for element in self.client:
+            if element == '.':
+                i += 1
+            else:
+                try:
+                    int(element)
+                except:
+                    valid = False
+                    pass
+        if not i == 3:
+            valid = False
+        return valid
+
+    def main(self):
         print('\n [1] ICMP Ping, [2] IP lookup, [3] Local IP\n')
         option = int(input('\n [?] Option: '))
+        if option == '1':
+            ping(self.client)
+        elif option == '2':
+            IP().lookup()
+        elif option == '3':
+            IP().portscan()
+
+    def ping(self):
 
 if __name__ == '__main__':
-    IP().main()
+    client = input('\n [?] Enter an IP address: ')
+    IP().valid()
